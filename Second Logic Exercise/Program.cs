a6c380e8dc57767dadf4af483d5248710c946f6d
borednuna <hanunshaka02@gmail.com>
2025-07-21 22:58:57 +0700

    feat: second logic exercise

---
+++ Second Logic Exercise/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Helper;
+
+const int DEFAULT_VALUE = 25;
+
+int x = DEFAULT_VALUE;
+
+if (args.Length >= 1)
+{
+    x = Convert.ToInt32(args[0]);
+}
+
+NumberHolder nh = new();
+nh.NumberChanged += Helper.Helper.NumberPrinter;
+
+for (int i = 1; i <= x; i++)
+{
+    nh.Number = i;
+    if (i < x) Console.Write(", ");
+}
+
+namespace Helper
+{
+    public delegate void NumberChangedHandler(int number);
+
+    public class NumberHolder
+    {
+        int _number = 1;
+        public int Number
+        {
+            get => _number;
+            set
+            {
+                _number = value;
+                NumberChanged?.Invoke(_number);
+            }
+        }
+        public NumberChangedHandler? NumberChanged;
+    }
+
+    public static class Helper
+    {
+        public static void NumberPrinter(int x)
+        {
+            if (x % 3 == 0) Console.Write("foo");
+            if (x % 5 == 0) Console.Write("bar");
+            if (x % 7 == 0) Console.Write("jazz");
+            if (!(x % 3 == 0 || x % 5 == 0 || x % 7 == 0)) Console.Write(x);
+        }
+    }
+}
